bf74f09fd9b4f2977684dff07ed5368909dcbd1a
laffingragons <akshaypatil,vit@gmail.com>
2018-01-03 21:40:47 +0530

    done

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 /////////routerlevel middleware////////////
 var express = require('express');
 var app = express();
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json({ limit: '10mb',extended: true}));
 app.use(bodyParser.urlencoded({limit: '10mb',extended: true}));
+
+var middleWare = require('./myMiddleware.js');
+
+////////start of route////////////////
+
+app.get('/normal/route', function (req, res) {
+    var dateOfBirth = new Date(req.query.dob);
+    res.send("This is a Normal route accessible to everyone and Date of birth is " + dateOfBirth);
+}); // end of normal route
+
+app.get('/restricted/route', middleWare.ageFilter, function (req, res) {
+    var dateOfBirth = new Date(req.query.dob);
+    res.send('This is a restricted route, you have to get reallly old for accessing this..')
+});
