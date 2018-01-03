42002d0f256a56fde900d71552d93825ccd0ad08
laffingragons <akshaypatil,vit@gmail.com>
2018-01-03 21:19:35 +0530

    done with birthdate logic

---
+++ myMiddleware.js
@@ -0,0 +1,28 @@
+// function to check age
+
+function calculateAge(dateOfBirth) {
+
+    var today = new Date();
+    var birthDate = new Date(dateOfBirth);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+// end calculateAge
+
+////now lets put the age fitler
+
+export.ageFilter = function (req, res, next) {
+
+    var age = calculateAge(req.query.dob);
+    console.log(age);
+    if (age >= 18) {
+        console.log("Age is Fine");
+        next();
+    } else {
+        res.send("Just few years and you will Accessing this page...");
+    }
+};
